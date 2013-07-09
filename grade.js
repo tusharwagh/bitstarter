d6ab40cbc14b46f451b72de5a10c47bed6f97691
Ubuntu <ubuntu@ip-10-154-173-188.ec2.internal>
2013-07-09 20:26:21 +0000

    Updated for part3 exercise using url

--- grade.js
+++ grade.js
@@ -20,16 +20,38 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
+var path = require('path');
 var fs = require('fs');
+var sys = require('util');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLFILE_URL = "googleoutput.html";
+var URL_DEFAULT = "http://www.google.com";
 
+var buildfn = function() {
+    var response2file = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            console.error("Wrote %s", HTMLFILE_URL);
+            fs.writeFileSync(HTMLFILE_URL, result);
+        }
+    };
+    return response2file;
+}
+
+var readUrl= function(url) {
+   var response2file = buildfn();
+   rest.get(url).on('complete', response2file);
+};
+    
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
+    if(!path.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
@@ -69,9 +91,10 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --url <url>', 'Url to retreive the content', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    readUrl(program.url); 
+    var checkJson = checkHtmlFile(HTMLFILE_URL, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
